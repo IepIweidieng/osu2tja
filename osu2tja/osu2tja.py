Moshivar/osz2tja2osz

Include MAKER in generated file

osu2tja/osu2tja.py
@@ -602,6 +602,7 @@ def osu2tja(fp, course, level, audio_name):
     tja_heads.append("BPM:%.2f" % timingpoints[0]["bpm"])
     tja_heads.append("OFFSET:-%.3f" % (timingpoints[0]["offset"] / 1000.0))
     tja_heads.append("DEMOSTART:%.3f" % (preview / 1000.0))
+    tja_heads.append("MAKER:%s" % creator)
 
     tja_contents.append("")
     tja_contents.append("COURSE:%s" % course)  # TODO: GUESS DIFFICULTY

IepIweidieng/osu2tja

osu2tja/osu2tja.py
@@ -729,6 +729,7 @@ def osu2tja(fp: IO[str], course: Union[str, int], level: Union[int, float], audi
         subtitle = f"{artist} ｢{subtitle}｣より"
     tja_heads_meta.append("SUBTITLE:--%s" % (subtitle or artist))
     tja_heads_meta.append("WAVE:%s" % (audio_name or audio))
+    tja_heads_meta.append("MAKER:%s" % creator)
 
     tja_heads_meta.append("DEMOSTART:%.3f" % (preview / 1000.0))
 
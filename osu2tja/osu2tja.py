031ecc59aaddb482afb7bdc4a7c63805087ee72e
Moshir <alexandermoshir@gmail.com>
2024-10-22 15:31:26 -1000

    Include MAKER in generated file

--- osu2tja/osu2tja.py
+++ osu2tja/osu2tja.py
@@ -729,6 +729,7 @@ def osu2tja(fp: IO[str], course: Union[str, int], level: Union[int, float], audi
         subtitle = f"{artist} ｢{subtitle}｣より"
     tja_heads_meta.append("SUBTITLE:--%s" % (subtitle or artist))
     tja_heads_meta.append("WAVE:%s" % (audio_name or audio))
+    tja_heads_meta.append("MAKER:%s" % creator)
 
     tja_heads_meta.append("DEMOSTART:%.3f" % (preview / 1000.0))
 
